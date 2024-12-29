35b74a038c038cc78484cda11faaf1f0968805d2
eterronesb <sucir1993@gmail.com>
2024-12-29 12:29:34 +0100

    Creamos el objeto player para que se pueda crear y se haga el ranking actualizable

---
+++ src/Player.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Player {
+    private String nickname;
+    private int score;
+
+    public Player(String nickname, int score) {
+        this.nickname = nickname;
+        this.score = score;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+    public int getScore() {
+        return score;
+    }
+
+    public void saveRanking() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("ranking.dat"))) {
+            List<Player> ranking = (List<Player>) ois.readObject();
+            ranking.add(this);
+            ranking.sort((p1, p2) -> Integer.compare(p2.getScore(), p1.getScore()));
+        //Mantendremos solo los 5 mejores
+            if (ranking.size() > 5) {
+                ranking = ranking.subList(0, 5);
+            }
+
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("ranking.dat"))) {
+                oos.writeObject(ranking);
+            }
+        }catch (Exception e) {
+            System.out.println("Error al guardar el ranking.");
+        }
+    }
+}
